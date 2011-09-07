48303ecdbb328b197e022cf53f53dd522abfc44a
Ozan Çağlayan <ozan@pardus.org.tr>
2011-09-07 15:19:34 +0300

    Add distutils stuff

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2011 TUBITAK/UEKAE
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; either version 2 of the License, or (at your
+# option) any later version. Please read the COPYING file.
+
+import os
+import sys
+import glob
+import shutil
+
+from distutils.core import setup
+
+# Call distutils.setup
+
+setup(name="brother-driver-installer",
+      version="0.1",
+      description="Simple tool to install Brother printer drivers",
+      long_description="A GUI tool for installing Brother printer drivers over the Internet",
+      author="Ozan Çağlayan",
+      author_email="ozan@pardus.org.tr",
+      url="https://github.com/ozancaglayan/install-brother-printer-driver",
+      license="GPLv2",
+      platforms=["Linux"],
+      scripts=["brother-driver-installer"],
+      )
